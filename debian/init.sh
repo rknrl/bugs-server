2c659259f750c601bf179ed7ff55cd6d924cac1f
tolyayanot <tolyayanot@gmail.com>
2017-02-23 19:49:45 +0300

    debian init note

--- debian/init.sh
+++ debian/init.sh
@@ -1,12 +1,13 @@
-#!/usr/bin/env bash
-
 host=mysite.com
+
+# ЛОКАЛЬНО:
 
 scp service/bugs-start $host:/usr/local/bin/bugs-start &&
 scp service/bugs-stop $host:/usr/local/bin/bugs-stop &&
 scp service/bugs $host:/etc/init.d/bugs &&
 
-ssh $host '
+# НА СЕРВЕРЕ:
+
 apt-get update &&
 
 apt-get install mysql-server -y &&
@@ -21,7 +22,7 @@ rm jdk-8u121-linux-x64.tar.gz &&
 
 update-alternatives --install /usr/bin/java java /opt/jdk/jdk1.8.0_121/bin/java 100 &&
 
-update-alternatives --install /usr/bin/javac javac /opt/jdk/jdk1.8.0_121/bin/javac 100
+update-alternatives --install /usr/bin/javac javac /opt/jdk/jdk1.8.0_121/bin/javac 100 &&
 
 mkdir /var/bugs &&
 
@@ -29,15 +30,18 @@ chmod +x /usr/local/bin/bugs-start &&
 chmod +x /usr/local/bin/bugs-stop &&
 chmod +x /etc/init.d/bugs &&
 update-rc.d bugs defaults
-' &&
 
-scp update_server.sh $host:/var/bugs &&
-scp rollback_server.sh $host:/var/bugs
-
-
-# mysql -u root -p
+mysql -u root -p
 # CREATE DATABASE bugs;
 # apply schema.sql
 # CREATE USER 'bugs_user'@'%' IDENTIFIED BY 'password';
 # GRANT ALL PRIVILEGES ON bugs TO 'bugs_user'@'%';
 # FLUSH PRIVILEGES;
+
+nano /etc/mysql/my.cnf
+# change line to: bind-address = $host
+
+# ЛОКАЛЬНО:
+
+scp update_server.sh $host:/var/bugs &&
+scp rollback_server.sh $host:/var/bugs
