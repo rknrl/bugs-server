38ce209f50ca073510fc405c0a95982fc03eccb5
tolyayanot <tolyayanot@gmail.com>
2017-02-27 18:16:02 +0300

    crossdomain.xml

--- src/main/scala-2.12/ru/rknrl/bugs/Server.scala
+++ src/main/scala-2.12/ru/rknrl/bugs/Server.scala
@@ -9,7 +9,8 @@
 package ru.rknrl.bugs
 
 import akka.event.slf4j.Logger
-import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.model.ContentTypes.`text/xml(UTF-8)`
+import akka.http.scaladsl.model.{HttpEntity, StatusCodes}
 import akka.http.scaladsl.server.Directives._
 import ru.rknrl.bugs.Utils._
 
@@ -20,29 +21,41 @@ object Server {
     if (arr.length == 2 && arr.head == paramName) arr.last else ""
   }
 
+  val crossdomain = "<?xml version=\"1.0\"?>\n" +
+    "<!DOCTYPE cross-domain-policy SYSTEM \"/xml/dtds/cross-domain-policy.dtd\">\n" +
+    "<cross-domain-policy>\n" +
+    "    <site-control permitted-cross-domain-policies=\"master-only\"/>\n" +
+    "    <allow-access-from domain=\"*\" to-ports=\"*\"/>\n" +
+    "</cross-domain-policy>"
+
   val logger = Logger("Server")
 
   def main(args: Array[String]): Unit =
     app(args(0), (config, connection, executionContext) ⇒
-      post {
-        path("bug") {
-          entity(as[String]) { string ⇒
-            if (logger.isDebugEnabled) logger.debug("Receive bug")
-            val lines = string.lines
-            val secret = getNextParam(lines, "secret")
-            val app = getNextParam(lines, "app")
-            val bug = getNextParam(lines, "bug")
+      get {
+        path("crossdomain.xml") {
+          complete(HttpEntity(`text/xml(UTF-8)`, crossdomain))
+        }
+      } ~
+        post {
+          path("bug") {
+            entity(as[String]) { string ⇒
+              if (logger.isDebugEnabled) logger.debug("Receive bug")
+              val lines = string.lines
+              val secret = getNextParam(lines, "secret")
+              val app = getNextParam(lines, "app")
+              val bug = getNextParam(lines, "bug")
 
-            if (secret == config.secret) {
-              connection.sendPreparedStatement("INSERT INTO bugs (time, app, bug, text) VALUES (NOW(), ?, ?, ?)", Seq(app, bug, string))
-              complete(StatusCodes.OK)
-            } else {
-              if (logger.isDebugEnabled) logger.debug("Wrong secret")
-              complete(StatusCodes.BadRequest)
+              if (secret == config.secret) {
+                connection.sendPreparedStatement("INSERT INTO bugs (time, app, bug, text) VALUES (NOW(), ?, ?, ?)", Seq(app, bug, string))
+                complete(StatusCodes.OK)
+              } else {
+                if (logger.isDebugEnabled) logger.debug("Wrong secret")
+                complete(StatusCodes.BadRequest)
+              }
             }
           }
         }
-      }
     )
 }
 
